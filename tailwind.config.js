a6e90d8826a17bb046adf7f619b6f43faa60e404
Haifaa-t <h.aloraini-t@tamkeentech.sa>
2025-05-08 13:24:58 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: 'class', // أهم سطر لتفعيل الثيم الداكن
+    content: [
+      './app/**/*.{js,ts,jsx,tsx}',
+      './components/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
+  
